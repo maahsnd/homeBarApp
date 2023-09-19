ae594d08dd6740084534113014da887efa105967
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-19 11:14:49 -0700

    Move optional fields to end

--- models/alcohol.js
+++ models/alcohol.js
@@ -6,8 +6,8 @@ const AlcoholSchema = new Schema({
   name: { type: String, required: true, minLength: 2, maxLength: 30 },
   description: { type: String, required: true, minLength: 20, maxLength: 100 },
   category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
-  price: { type: Number, required: false },
   fluid_volume: { type: Number, required: true },
+  price: { type: Number, required: false },
   date_opened: { type: Date, required: false }
 });
 
