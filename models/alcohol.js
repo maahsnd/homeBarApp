2b01dbaa668594de01bd46ad222503e58270c1ca
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-19 11:41:43 -0700

    Make price field optional

--- models/alcohol.js
+++ models/alcohol.js
@@ -6,7 +6,7 @@ const AlcoholSchema = new Schema({
   name: { type: String, required: true, minLength: 2, maxLength: 30 },
   description: { type: String, required: true, minLength: 20, maxLength: 100 },
   category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
-  fluid_volume: { type: Number, required: true },
+  fluid_volume: { type: Number, required: false },
   price: { type: Number, required: false },
   date_opened: { type: Date, required: false }
 });
