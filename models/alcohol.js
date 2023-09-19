abb24300ed6d532adfd78ae384e58c7441d51fcc
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-19 10:24:17 -0700

    Refine schema

--- models/alcohol.js
+++ models/alcohol.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
-const asyncHandler = require('express-async-handler');
 
 const Schema = mongoose.Schema;
 
 const AlcoholSchema = new Schema({
-  name: { type: String, required: true, maxLength: 30 },
-  description: { type: String, required: true, maxLength: 100 },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  name: { type: String, required: true, minLength: 2, maxLength: 30 },
+  description: { type: String, required: true, minLength: 20, maxLength: 100 },
+  category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
   price: { type: Number, required: false },
   fluidvolume: { type: Number, required: true },
   dateopened: { type: Date, required: false }
 });
 
 AlcoholSchema.virtual('url').get(function () {
-  return `/inventory/${this._id}`;
+  return `/inventory/alcohol/${this._id}`;
 });
