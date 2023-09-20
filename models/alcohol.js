e7819df1ab6a16cde2e5c03adf01bb751060462e
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-20 15:35:26 -0700

    Require category

--- models/alcohol.js
+++ models/alcohol.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const AlcoholSchema = new Schema({
   name: { type: String, required: true, minLength: 2, maxLength: 30 },
   description: { type: String, required: true, minLength: 20, maxLength: 500 },
-  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
   provenance: { type: String, required: false }
 });
 
