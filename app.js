9bcd433087450cc277c68093774a902756077ac6
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-20 11:09:17 -0700

    Add inventory router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const dotenv = require('dotenv').config();
 
 const indexRouter = require('./routes/index');
+const inventoryRouter = require('./routes/inventory');
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/inventory', inventoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
