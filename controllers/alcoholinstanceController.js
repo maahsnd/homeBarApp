7c802128525ce5e2b82e4c2eb0b0a58663ae9788
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-21 14:24:31 -0700

    Implement create inst put method

--- controllers/alcoholinstanceController.js
+++ controllers/alcoholinstanceController.js
@@ -1,7 +1,9 @@
-const alcohol = require('../models/alcohol');
+const Alcohol = require('../models/alcohol');
+const Location = require('../models/location');
 const { findById } = require('../models/alcohol');
-const alcoholInstance = require('../models/alcohol_instance');
+const AlcoholInstance = require('../models/alcohol_instance');
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
 
 //Display list of all alcoholInstances
 exports.alcoholInstance_list = asyncHandler(async (req, res, next) => {
@@ -23,8 +25,7 @@ exports.alcoholInstance_list = asyncHandler(async (req, res, next) => {
 
 // Display detail page for a specific alcoholInstance.
 exports.alcoholInstance_detail = asyncHandler(async (req, res, next) => {
-  const alcohol_inst = await alcoholInstance
-    .findById(req.params.id)
+  const alcohol_inst = await AlcoholInstance.findById(req.params.id)
     .populate('alcohol location')
     .populate({
       path: 'alcohol',
@@ -39,13 +40,74 @@ exports.alcoholInstance_detail = asyncHandler(async (req, res, next) => {
 
 // Display alcoholInstance create form on GET.
 exports.alcoholInstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: alcoholInstance create GET');
+  const [allAlcohols, allLocations] = await Promise.all([
+    Alcohol.find({}, 'name').sort({ name: 1 }).exec(),
+    Location.find({}, 'name').sort({ name: 1 }).exec()
+  ]);
+  res.render('alcoholinst_form', {
+    title: 'Create new acohol instance (bottle)',
+    locations: allLocations,
+    all_alcohols: allAlcohols
+  });
 });
 
 // Handle alcoholInstance create on POST.
-exports.alcoholInstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: alcoholInstance create POST');
-});
+exports.alcoholInstance_create_post = [
+  //location to array
+  (req, res, next) => {
+    if (!(req.body.location instanceof Array)) {
+      if (typeof req.body.location === 'undefined') req.body.location = [];
+      else req.body.location = new Array(req.body.location);
+    }
+    next();
+  },
+  //alcohol to array
+  (req, res, next) => {
+    if (!(req.body.alcohol instanceof Array)) {
+      if (typeof req.body.alcohol === 'undefined') req.body.alcohol = [];
+      else req.body.alcohol = new Array(req.body.alcohol);
+    }
+    next();
+  },
+  body('alcohol.*').escape(),
+  body('location.*').escape(),
+  body('date_opened', 'Date invalid')
+    .optional({ values: 'falsy' })
+    .isISO8601()
+    .toDate(),
+  body('fluid_volume').trim().escape(),
+  body('price').trim().escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const alcoholInst = new AlcoholInstance({
+      alcohol: req.body.alcohol,
+      location: req.body.location,
+      date_opened: req.body.date_opened,
+      fluid_volume: req.body.fluid_volume,
+      price: req.body.price
+    });
+
+    if (!errors.isEmpty()) {
+      const [allLocations, allAlcohols] = await Promise.all([
+        Location.find({}, 'name').sort({ name: 1 }).exec(),
+        Alcohol.find({}, 'name').sort({ name: 1 }).exec()
+      ]);
+      res.render('alcoholinst_form', {
+        title: 'Create new acohol instance (bottle)',
+        locations: allLocations,
+        all_alcohols: allAlcohols,
+        selected_alcohol: alcoholInst
+      });
+      return;
+    } else {
+      await alcoholInst.save();
+
+      res.redirect(alcoholInst.url);
+    }
+  })
+];
 
 // Display alcoholInstance delete form on GET.
 exports.alcoholInstance_delete_get = asyncHandler(async (req, res, next) => {
