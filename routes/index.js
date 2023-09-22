fbd4d9688a545786fff39c4137bb93522fb3827b
mrwindupbirdesq <nmaahs11@gmail.com>
2023-09-22 15:15:27 -0700

    Add extremely basic password protection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const dotenv = require('dotenv').config();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  res.redirect('/inventory');
+  res.render('login');
+});
+
+router.post('/', function (req, res) {
+  const password = req.body.password;
+  if (password === process.env.ACCESSPASSWORD) {
+    res.redirect('/inventory');
+  } else {
+    res.render('login', { error: 'Access denied' });
+  }
 });
 
 module.exports = router;
